cc2805eb9456e94b1a40dccb8454f2259f1e7607
Oliver Zernikow <zernikow@uni-potsdam.de>
2022-01-18 16:51:33 +0100

    reward punishment, bug fixing

--- agent.py
+++ agent.py
@@ -15,16 +15,15 @@ MINE_COUNT = 10
 
 class Agent:
     def __init__(self):
-        self.gamma = 0
+        self.gamma = 0.9
         self.epsilon = 0
-        self.number_of_games = 0.9
+        self.number_of_games = 0
         self.memory = deque(maxlen=MAX_MEMORY)
         self.model = Linear_QNet(GAME_SIZE*GAME_SIZE, 256, GAME_SIZE*GAME_SIZE)
         self.trainer = QTrainer(self.model, LEARNING_RATE, self.gamma)
 
     def getAction(self, state, game_size, game_instance: ms.Minesweeper):
-        self.epsilon = 80 - self.number_of_games
-        move = (0,0)
+        self.epsilon = 100 - self.number_of_games
         if random.randint(0, 200) < self.epsilon:
             while True:
                 i = random.randint(0, game_size-1)
@@ -32,13 +31,14 @@ class Agent:
                 if not game_instance.field[i,j]:
                     move = (i,j)
                     break
-        else:
-            state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model(state0)
-            _move = torch.argmax(prediction).item()
-            move = np.unravel_index(move, game_instance.field.shape)
+        
+            return move, True
 
-        return move
+        state0 = torch.tensor(state, dtype=torch.float)
+        prediction = self.model(state0)
+        argmax_move = torch.argmax(prediction).item()
+        move = np.unravel_index(argmax_move, game_instance.field.shape)
+        return move, False
 
     def _getActions(self, game_instance: ms.Minesweeper):
         return np.invert(game_instance.field)
@@ -56,7 +56,7 @@ class Agent:
             mini_sample = self.memory
 
         states, new_states, moves, rewards, dones = zip(*mini_sample)
-        self.trainer.train_step(states,- new_states, moves, rewards, dones)
+        self.trainer.train_step(states, new_states, moves, rewards, dones)
 
 
     def getState(self, game_instance: ms.Minesweeper):
@@ -65,9 +65,10 @@ class Agent:
         for i in range(0, game_instance.game_size):
             for j in range(0, game_instance.game_size):
                 if game_instance.field[i,j]:
-                    out[i,j] = game_instance.field_assignment[i,j]
+                    #out[i,j] = game_instance.field_assignment[i,j]
+                    out[i,j] = 1 #anstatt die tatsaechlichen werte aus dem spiel zu verwenden, setzen wir aufgedeckt=1
 
-        return out
+        return out.reshape((1,81))
 
 
 def train():
@@ -77,22 +78,33 @@ def train():
     plot_mean_scores = []
     agent = Agent()
     game = ms.Minesweeper(GAME_SIZE, MINE_COUNT)
+    random_counter = 0
+    field_already_unfolded_multiplier = 1
 
     while True:
         state = agent.getState(game)
-        move = agent.getAction(state, GAME_SIZE, game) #the next move aka field to click on
+        move, random_move = agent.getAction(state, GAME_SIZE, game) #the next move aka field to click on
+        random_counter += random_move
+
         old_score = game.unfolded
-        isLost = game.unfold(move[0], move[1])
+        field_already_unfolded = game.field[move[0], move[1]]
+        isLost = not game.unfold(move[0], move[1])
         score = game.unfolded
         reward = (score - old_score) #mehr reward bei mehr feldern, die auf einmal aufgedeckt wurden?
 
+        if field_already_unfolded:
+            reward = -2 * field_already_unfolded_multiplier 
+            field_already_unfolded_multiplier += 1
+        elif isLost:
+            reward = -4
+        
         state_new = agent.getState(game)
 
         agent.train_short_memory(state, state_new, move, reward, isLost)
 
         agent.remember(state, state_new, move, reward, isLost)
 
-        if isLost:
+        if game.is_game_won() or isLost:
             game = ms.Minesweeper(GAME_SIZE, MINE_COUNT)
             agent.number_of_games += 1
             agent.train_long_memory()
@@ -101,8 +113,22 @@ def train():
                 high_score = score
                 agent.model.save()
 
-            print("Game", agent.number_of_games, "score", score, "highscore", high_score)
+            if game.is_game_won():
+                print(f"+++++++++ WIR HABEN GEWONNEN🔫🔫🔫 +++++++")
+                print(f"+++++++++ WIR HABEN GEWONNEN🔫🔫🔫 +++++++")
 
+                print(f"+++++++++ WIR HABEN GEWONNEN🔫🔫🔫 +++++++")
+
+                print(f"+++++++++ WIR HABEN GEWONNEN🔫🔫🔫 +++++++")
+
+                print(f"+++++++++ WIR HABEN GEWONNEN🔫🔫🔫 +++++++")
+
+                print(f"+++++++++ WIR HABEN GEWONNEN🔫🔫🔫 +++++++")
+                print()
+
+            print("Game", agent.number_of_games, "score", score, "highscore", high_score, "number of random moves", random_counter)
+            random_counter = 0
+            field_already_unfolded_multiplier = 1
             
 
 if __name__ == "__main__":
