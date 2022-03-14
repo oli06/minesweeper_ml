84c001f7b1610212fe3c26659e32ef7b45856c93
lkursawe <lara.kursawe@uni-potsdam.de>
2022-03-14 16:33:06 +0100

    more epsilon plot additions

--- Epsilon_Plot/epsilon_plt_totalwins.py
+++ Epsilon_Plot/epsilon_plt_totalwins.py
@@ -3,18 +3,31 @@ import matplotlib.pyplot as plt
 
 with open('epsilon90.txt') as f:
     lines = f.readlines()
-    x1 = [i * 100 for i in range(len(lines[:101]))]
-    y1 = [float(line.split(',')[0]) for line in lines[:101]]
+    x1 = [i * 100 for i in range(len(lines[:3]))]
+    total = 0
+    y1 = []
+    for line in lines[:3]:
+        total += float(line.split(',')[0])
+        y1.append(total)
 
 with open('epsilon35.txt') as g:
     lines = g.readlines()
-    x2 = [i * 100 for i in range(len(lines[:101]))]
-    y2 = [float(line.split(',')[0]) for line in lines[:101]]
+    x2 = [i * 100 for i in range(len(lines[:3]))]
+    total = 0
+    y2 = []
+    for line in lines[:3]:
+        total += float(line.split(',')[0])
+        y2.append(total)
 
 with open('epsilon1.txt') as h:
     lines = h.readlines()
-    x3 = [i * 100 for i in range(len(lines[:101]))]
-    y3 = [float(line.split(',')[0]) for line in lines[:101]]        
+    x3 = [i * 100 for i in range(len(lines[:3]))]
+    total = 0
+    y3 = []
+    for line in lines[:3]:
+        total += float(line.split(',')[0])
+        y3.append(total)
+
 
 fig = plt.figure()
 
@@ -24,7 +37,7 @@ ax1.set_xlabel('games')
 ax1.set_ylabel('wins per 100 games')
 ax1.plot(x1,y1, c='r', label='epsilon = 0.9')
 #ax1.set_xscale('log')
-#ax1.set_yscale('symlog')
+#ax1.set_yscale('log')
 
 
 ax2 = fig.add_subplot(111) 
@@ -32,7 +45,7 @@ ax2.set_xlabel('games')
 ax2.set_ylabel('wins per 100 games')
 ax2.plot(x2,y2, c='b', label='epsilon = 0.35')
 #ax2.set_xscale('log')
-#ax2.set_yscale('symlog')
+#ax2.set_yscale('log')
 
 
 ax3 = fig.add_subplot(111)
@@ -40,7 +53,7 @@ ax3.set_xlabel('games')
 ax3.set_ylabel('wins per 100 games')
 ax3.plot(x3,y3, c='g', label='epsilon = 0.01')
 #ax3.set_xscale('log')
-#ax3.set_yscale('symlog')
+#ax3.set_yscale('log')
 
 
 leg = ax1.legend()
---
+++ Epsilon_Plot/epsilon_plt_winrate.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open('epsilon90.txt') as f:
+    lines = f.readlines()
+    x1 = [i * 100 for i in range(len(lines[:10]))]
+    y1 = [float(line.split(',')[0]) for line in lines[:10]]
+
+with open('epsilon35.txt') as g:
+    lines = g.readlines()
+    x2 = [i * 100 for i in range(len(lines[:10]))]
+    y2 = [float(line.split(',')[0]) for line in lines[:10]]
+
+with open('epsilon1.txt') as h:
+    lines = h.readlines()
+    x3 = [i * 100 for i in range(len(lines[:10]))]
+    y3 = [float(line.split(',')[0]) for line in lines[:10]]        
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+ax1.set_title("winrate with different epsilons")    
+ax1.set_xlabel('games')
+ax1.set_ylabel('wins per 100 games')
+ax1.plot(x1,y1, c='r', label='epsilon = 0.9')
+#ax1.set_xscale('log')
+#ax1.set_yscale('symlog')
+
+
+ax2 = fig.add_subplot(111) 
+ax2.set_xlabel('games')
+ax2.set_ylabel('wins per 100 games')
+ax2.plot(x2,y2, c='b', label='epsilon = 0.35')
+#ax2.set_xscale('log')
+#ax2.set_yscale('symlog')
+
+
+ax3 = fig.add_subplot(111)
+ax3.set_xlabel('games')
+ax3.set_ylabel('wins per 100 games')
+ax3.plot(x3,y3, c='g', label='epsilon = 0.01')
+#ax3.set_xscale('log')
+#ax3.set_yscale('symlog')
+
+
+leg = ax1.legend()
+
+plt.show()
